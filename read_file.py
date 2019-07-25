58d52574c72f6ff1a1ec4d292a2db8805f2a8d9f
Sreekiran096 <kiranvadaga16@gmail.com>
2019-07-25 14:56:00 +0530

    Error files Print statement added

--- read_file.py
+++ read_file.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 13:48:21 2019
+
+@author: vadaga
+"""
+
 import pandas as pd
 import feather
 
@@ -8,7 +15,10 @@ def read_file(path):
         data = pd.read_excel(path)
     elif '.feather' in path:
         data = feather.read_dataframe(path)
+    else:
+        print("No file with the mentioned path {}".format(path))
     return data
 
-path = input('Path for the file to read : ')
+path = input('Path for the file to read: ')
+print("path is given {}".format(path))
 data = read_file(path)
